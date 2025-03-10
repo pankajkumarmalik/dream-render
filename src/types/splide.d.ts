7399d9063ab627794e4e805c82040261e057e4f7
Pankaj Kumar Malik <pankajkumarmalik6@gmail.com>
2025-03-10 17:45:53 +0530

    TypeScript changes in splide.d.ts for production build

--- src/types/splide.d.ts
+++ src/types/splide.d.ts
@@ -1,6 +1,7 @@
 declare module "@splidejs/react-splide" {
-  import { ComponentType } from "react";
-  export const Splide: ComponentType<any>;
-  export const SplideSlide: ComponentType<any>;
-  export const AutoScroll: any;
+  import { ComponentType, ReactNode } from "react";
+
+  export const Splide: ComponentType<{ children?: ReactNode }>;
+  export const SplideSlide: ComponentType<{ children?: ReactNode }>;
+  export const AutoScroll: Record<string, unknown>; // If AutoScroll is an object with unknown properties
 }
